be44c4460b8b5a7152c9a5f1410529c44b63c5d2
AlejandroDev8 <alejandrodeveloper417@gmail.com>
2022-12-30 02:33:07 -0600

    Agregando un par de temas más

---
+++ js/for.js
@@ -0,0 +1,28 @@
+// For
+
+// for (let i = 0; i < 10; i++) {
+//   console.log(i);
+// }
+
+// for (let i = 0; i <= 100; i++) {
+//   if (i % 2 === 0) {
+//     console.log(`El Número: ${i} es PAR`);
+//   } else {
+//     console.log(`El Número ${i} es IMPAR`);
+//   }
+// }
+
+const carrito = [
+  { nombre: "Monitor 20 pulgadas", precio: 500 },
+  { nombre: "Televisión 50 pulgadas", precio: 700 },
+  { nombre: "Tablet", precio: 300 },
+  { nombre: "Audífonos", precio: 200 },
+  { nombre: "Teclado", precio: 50 },
+  { nombre: "Celular", precio: 500 },
+  { nombre: "Bocinas", precio: 300 },
+  { nombre: "Laptop", precio: 800 },
+];
+
+for (let i = 0; i < carrito.length; i++) {
+  console.log(carrito[i].nombre);
+}
---
+++ js/switch.js
@@ -0,0 +1,18 @@
+// Switch
+
+const métodoPago = "efectivo";
+
+switch (métodoPago) {
+  case "tarjeta":
+    console.log("Pagaste con tarjeta");
+    break;
+  case "cheque":
+    console.log("El usuario va a pagar con cheque, revisaremos los fondos primero");
+    break;
+  case 'efectivo':
+    console.log('Pagaste con efectivo');
+    break;
+  default:
+    console.log("Aún no has pagado");
+    break;
+}
